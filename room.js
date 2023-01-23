02836ed10b54944d6b79eff7e39b5e0dd4688a40
HishamTareq <hishamtar3q@gmail.com>
2023-01-23 22:13:53 +0200

    Update room.js

--- room.js
+++ room.js
@@ -1,16 +1,38 @@
-var connections = {};
+class Command {
+  static PREFIX = "!";
+  constructor(name, syntax, id, admin, active) {
+    this.name = name;
+    this.syntax = syntax;
+    this.id = id;
+    this.admin = admin;
+    this.active = active;
+  }
+};
 
-var token = "thr1.AAAAAGPOyW82eiuFbphqaw.B4oIDPWs33w";
-var roomName = "NAME";
-var public = false;
-var noPlayer = true;
-var maxPlayers = 5;
+const connections = {};
 
-var CONNECTION_MODE = true;
-var RECAPTCHA_MODE = false;
-var BLACKLIST_MODE = true;
+const commands = [
+  new Command('help', /(\s+)?h(\s+)?e(\s+)?l(\s+)?p/i, 1, false, false),
+  new Command('bb', /(\s+)?b(\s+)?b/i, 2, false, false),
+  new Command('admin', /(\s+)?a(\s+)?d(\s+)?m(\s+)?i(\s+)?n/i, 3, false, false),
+  new Command('waive', /(\s+)?w(\s+)?a(\s+)?i(\s+)?v(\s+)?e/i, 4, false, false),
+  new Command('afk', /(\s+)?a(\s+)?f(\s+)?k/i, 5, false, false),
+  new Command('afks', /(\s+)?a(\s+)?f(\s+)?k(\s+)?s/i, 6, false, false),
+  new Command('mute', /(\s+)?m(\s+)?u(\s+)?t(\s+)?e(\s+)?#(\s+)?\d+/i, 7, false, false),
+  new Command('unmute', /(\s+)?u(\s+)?n(\s+)?m(\s+)?u(\s+)?t(\s+)?e(\s+)?#(\s+)?\d+/i, 8, false, false)
+];
 
-var room = HBInit({
+const token = "thr1.AAAAAGPOyW82eiuFbphqaw.B4oIDPWs33w";
+const roomName = "NAME";
+const public = false;
+const noPlayer = true;
+const maxPlayers = 5;
+
+let CONNECTION_MODE = true;
+let RECAPTCHA_MODE = false;
+let BLACKLIST_MODE = true;
+
+const room = HBInit({
   maxPlayers: maxPlayers,
   roomName: roomName,
   noPlayer: noPlayer,
@@ -24,9 +46,13 @@ room.onPlayerJoin = function (player) {
   check({ id: player.id, conn: player.conn });
 };
 
-onPlayerLeave = function (player) {
+room.onPlayerChat = function (player, message) {
+  
+};
+
+room.onPlayerLeave = function (player) {
   delete connections[player.id];
-}
+};
 
 Object.defineProperty(Object.prototype, 'swap', {
   value: function () {
@@ -48,6 +74,6 @@ function check(player) {
   var { [player.conn]: conn } = connections.swap();
   var blacklist = JSON.parse(window.localStorage.getItem('blacklist'));
   if (BLACKLIST_MODE && blacklist.find(p => p.conn == player.conn)) return room.kickPlayer(player.id, 'BLACKLISTED');
-  if (CONNECTION_MODE && conn) return room.kickPlayer(player.id);
+  if (CONNECTION_MODE && conn) return room.kickPlayer(player.id, 'It is not allowed to join more than one player from the same network');
   connections[player.id] = player.conn;
-}
+};
