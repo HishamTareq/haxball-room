229f1fafa94c5662cf396ee6d7aaba7f3071ebf0
HishamTareq <hishamtar3q@gmail.com>
2023-01-24 22:32:06 +0200

    Update room.js

--- room.js
+++ room.js
@@ -1,13 +1,24 @@
+/**
+ * An object that contains the current players `id` as a `key` and its `value` is `conn`
+ */
 var connections = {};
 
-var token = "thr1.AAAAAGPQD8KRxIrCHbJWSg.uuI6tluKNLk";
+var token = "thr1.AAAAAGPQNu4N_uq_S-RPiw.9oOzTcyHifA";
 var roomName = "NAME";
 var public = false;
 var noPlayer = true;
 var maxPlayers = 12;
-
+/**
+ * Enable and disable connection mode
+ */
 var CONNECTION_MODE = true;
+/**
+ * Enable and disable Recaptcha mode
+ */
 var RECAPTCHA_MODE = false;
+/**
+ * Enable and disable blacklist mode
+ */
 var BLACKLIST_MODE = true;
 
 var room = HBInit({
@@ -15,7 +26,7 @@ var room = HBInit({
   roomName: roomName,
   noPlayer: noPlayer,
   public: public,
-  token: token
+  token: token,
 });
 
 room.setRequireRecaptcha(RECAPTCHA_MODE);
@@ -27,6 +38,10 @@ room.onPlayerJoin = function (player) {
 room.onPlayerLeave = function (player) {
   delete connections[player.id];
 };
+
+room.onPlayerChat = function (player, message) {
+  
+}
 
 Object.defineProperty(connections, 'swap', {
   value: function () {
@@ -40,13 +55,13 @@ Object.defineProperty(connections, 'swap', {
 
 /**
  * Check if the player is blacklisted or try to join again from the same network,
- * will be kicked out if one of the above two conditions is met.
+ * will be kicked if one of the above two conditions is met.
  * @param {PlayerObject} player
  */
 
 function check(player) {
   var { [player.conn]: conn } = connections.swap();
-  var blacklist = JSON.parse(window.localStorage.getItem('blacklist'));
+  var blacklist = [];
   if (BLACKLIST_MODE && blacklist.find(p => p.conn == player.conn)) return room.kickPlayer(player.id, 'BLACKLISTED');
   if (CONNECTION_MODE && conn) return room.kickPlayer(player.id, 'It is not allowed to join more than one player from the same network');
   connections[player.id] = player.conn;
