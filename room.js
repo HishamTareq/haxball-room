d85b5bc11b090098644b692f848aa8f675edac83
HishamTareq <hishamtar3q@gmail.com>
2023-01-26 01:01:06 +0200

    Update room.js

--- room.js
+++ room.js
@@ -1,32 +1,19 @@
-/**
- * Enable or disable connection mode.
- */
-let CONNECTION_MODE = false;
-/**
- * Enable or disable Recaptcha mode.
- */
+let CONNECTION_MODE = true;
 let RECAPTCHA_MODE = false;
-/**
- * Enable or disable blacklist mode.
- */
 let BLACKLIST_MODE = true;
-/**
- * Enable or disable Automatic clearbans mode.
- */
 let CLEARBANS_MODE = true;
 
-let mainColor = 0xFF9800;
-let wrongColor = 0xE53935;
-let tipColor = 0xFF7043;
+let mainColor = 0x43A047;
+let secondaryColor = 0x8bc34a;
+let warningColor = 0xc62828;
+let tipColor = 0x9e9e9e;
 
-let token = "thr1.AAAAAGPRh2_qpgn7YLiOxQ.ZZjIDNrX3iE";
-let roomName = "a's room";
-let public = true;
+let token = "thr1.AAAAAGPRpOIj_py6ZQaRuw.3IaKyN8Jid8";
+let roomName = "room name";
+let public = false;
 let noPlayer = true;
 let maxPlayers = 5;
-/**
- * An object that contains the current players `id` as a `key` and its `value` is `conn`.
- */
+
 const connections = {};
 
 const commands = {
@@ -59,7 +46,7 @@ const commands = {
       admin: true,
       display: false,
     },
-  ]
+  ],
 }
 
 const room = HBInit({
@@ -68,14 +55,13 @@ const room = HBInit({
   noPlayer: noPlayer,
   public: public,
   token: token,
-  password: "111",
 });
 
 room.setRequireRecaptcha(RECAPTCHA_MODE);
 
 room.onPlayerJoin = function (player) {
   check({ id: player.id, conn: player.conn });
-  CLEARBANS_MODE && room.sendAnnouncement("Banlist is emptied every 10 minutes, players who abuse permissions will be blacklisted 📌\nbecause there are no moderators in the room at the moment, so be careful!", player.id, 0x757575, "small");
+  CLEARBANS_MODE && room.sendAnnouncement("In this demo room, banlist is emptied every 10 minutes, players who abuse permissions will be blacklisted 📌\nbecause there are no moderators in the room at the moment, so be careful!", player.id, 0x757575, "small");
   room.sendAnnouncement("Welcome " + player.name + ", Check " + commands.char + "help to show public commands", player.id, tipColor);
 };
 
@@ -93,11 +79,15 @@ room.onPlayerChat = function (player, message) {
     let command = getCommandBySyntax(message);
     if (!command) return false;
     if (command.admin && !player.admin) {
-      room.sendAnnouncement("You are not admin", player.id, wrongColor, null, 2);
+      room.sendAnnouncement("You are not admin", player.id, warningColor, null, 2);
       return false;
     }
-    run(command, player);
+    run(command, player, message);
     return command.display;
+  }
+  if (isMutedPlayer(player.id)) {
+    room.sendAnnouncement("You are muted", player.id, warningColor, null, 2);
+    return false;
   }
 }
 
@@ -148,7 +138,7 @@ function checkCommandSyntax(message) {
 
 function getCommandBySyntax(message) {
   var message = message.slice(1);
-  return [...commands.public].find(c => message.match(c.syntax)?.[0] == message);
+  return [...commands.public, ...commands.private].find(c => message.match(c.syntax)?.[0] == message);
 };
 
 /**
@@ -157,21 +147,21 @@ function getCommandBySyntax(message) {
  * @param { PlayerObject } player
  */
 
-function run(command, player) {
+function run(command, player, message) {
   switch (command.id) {
     case 1:
       const formatter = new Intl.ListFormat("en", { style: "short", type: "conjunction" });
       room.sendAnnouncement("Commands: " + formatter.format(commands.public.map((c) => commands.char + c.name)), player.id, mainColor);
-    break;
+      break;
     case 2:
       room.kickPlayer(player.id, "Good Bye!");
-    break;
+      break;
     case 3:
       room.setPlayerAdmin(player.id, false);
-    break;
+      break;
     case 4:
       room.clearBans();
       room.sendAnnouncement("Banlist has been cleared by @" + player.name, null, mainColor);
-    break;
+      break;
   }
 };
